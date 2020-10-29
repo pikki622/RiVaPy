8a4ad7f165fba302bbaa4b74cd0dd6f6b36e799f
Daniel Oeltz <d.oeltz@rivacon.com>
2020-10-29 08:51:34 +0100

    admin

--- rivapy/pricing/pricer.py
+++ rivapy/pricing/pricer.py
@@ -1,7 +1,12 @@
-from pyvacon.pricing import price as _price
+from rivapy import _pyvacon_available
+if _pyvacon_available:
+    from pyvacon.pricing import price as _price
 
 def price(pr_data):
     if hasattr(pr_data, 'price'):
         return pr_data.price()
     else:
-        return _price(pr_data)
+        if _pyvacon_available:
+            return _price(pr_data)
+        else:
+            raise Exception('Cannot price: pyvacon not avilable.')

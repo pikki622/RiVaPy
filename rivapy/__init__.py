288f17e755dc8f20269414a51ca4e6b1dfd0d1aa
doeltz <daniel.oeltz@scai.fraunhofer.de>
2020-11-13 14:45:26 +0100

    admin

--- rivapy/__init__.py
+++ rivapy/__init__.py
@@ -1,17 +1,13 @@
 import warnings
-    
-try:
-    import pyvacon.version as version
-    from pyvacon.analytics import setLogLevel as set_log_level
-    from pyvacon.analytics import registerSerialization as _register_serialization
-    import pyvacon.marketdata as marketdata
-    _register_serialization('depp')
-    _pyvacon_available = True
-    if version.is_beta:
-        warnings.warn('Imported pyvacon is just beta version.')
-except:
-    _pyvacon_available = False
-    warnings.warn('No pyvacon available, only limited functionality supported.')
+import pyvacon.version as version
+from pyvacon.analytics import setLogLevel as set_log_level
+from pyvacon.analytics import registerSerialization as _register_serialization
+import pyvacon.marketdata as marketdata
+_register_serialization('depp')
+_pyvacon_available = True
+if version.is_beta:
+    warnings.warn('Imported pyvacon is just beta version.')
+
 
 from rivapy import enums
 import rivapy.instruments as instruments

8bd431c74c2d9b4ccd0622ef278d0e40190160c0
Daniel Oeltz <d.oeltz@rivacon.com>
2020-11-21 06:43:35 +0100

    admin

--- rivapy/instruments/__init__.py
+++ rivapy/instruments/__init__.py
@@ -1,2 +1,2 @@
-from rivapy.instruments.specifications import *
-from rivapy.instruments.cds_specification import CDSSpecification
+from RiVaPy.instruments.specifications import *
+from RiVaPy.instruments.cds_specification import CDSSpecification

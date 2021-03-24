dd94d79ac840ee202b84bc497b2627f05ce7ec99
maxm23 <max@marg-germany.de>
2021-03-24 17:30:08 +0100

    admin

--- rivapy/pricing/pricing_data.py
+++ rivapy/pricing/pricing_data.py
@@ -110,6 +110,48 @@ class CDSPricingData:
                 premium_period_start = premium_payment
         return risk_adj_factor_premium, accrued
 
+    def par_spread(self, valuation_date: datetime, integration_stepsize: relativedelta)->float:
+        prev_date = max(self.val_date, self.spec.protection_start)
+        current_date = min(prev_date + self.integration_step, self.spec.expiry)
+        pv_protection = 0.0
+        premium_period_start = self.spec.protection_start
+        risk_adj_factor_premium=0  
+        accrued = 0 
+
+        while current_date <= self.spec.expiry:
+            default_prob = self.survival_curve.value(valuation_date, prev_date)-self.survival_curve.value(valuation_date, current_date)
+            recovery = self.spec.recovery
+            if recovery is None and self.recovery_curve is not None:
+                recovery = self.recovery_curve.value(valuation_date, current_date) 
+            pv_protection += self.discount_curve.value(valuation_date, current_date) * (1.0-recovery) * default_prob
+            prev_date = current_date
+            current_date += self.integration_step
+            
+        if prev_date < self.spec.expiry and current_date > self.spec.expiry:
+            default_prob = self.survival_curve.value(valuation_date, prev_date)-self.survival_curve.value(valuation_date, self.spec.expiry)
+            recovery = self.spec.recovery
+            if recovery is None and self.recovery_curve is not None:
+                recovery = self.recovery_curve.value(valuation_date, self.spec.expiry) 
+            pv_protection += self.discount_curve.value(valuation_date, self.spec.expiry) * (1.0-recovery) * default_prob
+
+        dc = _pyvacon.finance.definition.DayCounter(_pyvacon.finance.definition.DayCounter.Type.Act365Fixed)
+        for premium_payment in self.spec.premium_pay_dates:
+            if premium_payment >= valuation_date:
+                period_length = dc.yf(premium_period_start, premium_payment)
+                survival_prob = self.survival_curve.value(valuation_date, premium_payment)
+                df = self.discount_curve.value(valuation_date, premium_payment)
+                risk_adj_factor_premium += period_length*survival_prob*df
+                default_prob = self.survival_curve.value(valuation_date, premium_period_start)-self.survival_curve.value(valuation_date, premium_payment)
+                accrued += period_length*default_prob*df
+                premium_period_start = premium_payment
+
+        PV_accrued=((1/2)*accrued)
+        PV_premium=(1)*risk_adj_factor_premium
+        PV_protection=(((1-recovery))*pv_protection)
+        
+        par_spread_i=(PV_protection)/((PV_premium+PV_accrued))
+        return par_spread_i
+
     def price(self):
         pv_protection = self._pv_protection_leg(self.val_date, self.integration_step)
         pr_results = PricingResults()
@@ -117,6 +159,6 @@ class CDSPricingData:
         premium_leg, accrued = self._pv_premium_leg(self.val_date)
         pr_results.premium_leg = self.spec.premium*self.spec.notional*premium_leg
         pr_results.accrued = 0.5*self.spec.premium*self.spec.notional*accrued
-        pr_results.par_spread=(pr_results.pv_protection)/(pr_results.premium_leg+pr_results.accrued)
+        pr_results.par_spread=self.par_spread(self.val_date, self.integration_step)
         pr_results.set_price(pr_results.pv_protection-pr_results.premium_leg-pr_results.accrued)
         return pr_results

73f66649563e362a8e3427079068f101990e136e
Daniel Oeltz <d.oeltz@rivacon.com>
2021-08-20 06:14:37 +0200

    admin

--- rivapy/models/__init__.py
+++ rivapy/models/__init__.py
@@ -1,22 +1,32 @@
+import bisect
 import numpy as np
 from numpy.core.fromnumeric import var
 import scipy.interpolate as interpolation
 import rivapy.numerics.kernel_regression as kernel_regression
+
+def _interpolate_2D(time_grid, strikes, f, x, t):
+    t_index = bisect.bisect_left(time_grid, t)
+    if t_index == 0:# or t0_index == self._time_grid.shape[0]:
+        result = f[0]
+    elif t_index == time_grid:
+        result = f[-1]
+    else:
+        dt = time_grid[t_index] - time_grid[t_index-1]
+        w1 = (t-time_grid[t_index-1])/dt
+        w2 = (time_grid[t_index] - t)/dt
+        result = w1*f[t_index] + w2*f[t_index-1]
+    return np.interp(x[:,0], strikes, result)
+    
 class LocalVol:
 
-    def __init__(self, vol_param, x_strikes: np.array, time_grid: np.array, drift: np.array):
+    def __init__(self, vol_param, x_strikes: np.array, time_grid: np.array):
         self._local_variance = LocalVol.compute_local_var(vol_param, x_strikes, time_grid)
         self._x_strikes = x_strikes
         self._time_grid = time_grid
-        self._variance = interpolation.interp2d(x_strikes, time_grid, self._local_variance)
-        self._drift = interpolation.interp1d(time_grid, drift)
-
-    def _get_SDE_coeff(self, t, x):
-        variance = self._variance(x, t)
-        drift = self._drift(t) - 0.5*variance
-        vol = np.sqrt(variance)
-        return drift, vol
-
+        self._variance = interpolation.interp2d(time_grid, x_strikes, self._local_variance.T)
+                        #interpolation.RectBivariateSpline(time_grid, x_strikes,  
+                         #               self._local_variance, bbox=[None, None, None, None], kx=1, ky=1, s=0)
+        
     @staticmethod
     def compute_local_var(vol_param, x_strikes: np.array, time_grid: np.array):
         # setup grids 
@@ -38,8 +48,8 @@ class LocalVol:
         fd2c = 2.0 / (hp*(hm + hp))
         fd2b = -(fd2a + fd2c)
 
-        min_lv = 0.001
-        max_lv = 2.5
+        min_lv = 0.01
+        max_lv = 1.5
         inv_dt = 1.0/(time_grid[1:]-time_grid[:-1])
 
         dyw = fd1a*tiv[:,:-2] + fd1b*tiv[:,1:-1] + fd1c*tiv[:,2:]
@@ -57,9 +67,30 @@ class LocalVol:
         return local_var
 
 
-    def apply_mc_step(self, x, t0, t1, rnd):
-        drift, vol = self._get_SDE_coeff(t0, x)
-        x*=np.exp(drift*(t1-t0) + vol*np.sqrt(t1-t0)*rnd)
+    def apply_mc_step(self, x: np.ndarray, t0: float, t1: float, rnd: np.ndarray, inplace: bool = True):
+        """Apply a MC-Euler step for the LV Model for n different paths.
+
+        Args:
+            x (np.ndarray): 2-d array containing the start values for the spot and variance. The first column contains the spot, the second the variance values.
+            t0 ([type]): [description]
+            t1 ([type]): [description]
+            rnd ([type]): [description]
+        """
+        if not inplace:
+            x_ = x.copy()
+        else:
+            x_ = x
+        S = x_[:,0]
+        #try:
+        lv = self._variance(t0, S).reshape((-1,)) #_interpolate_2D(self._time_grid, self._x_strikes, self._local_variance)
+        #except:
+            #return t0, S
+            #raise Exception()
+        dt = t1-t0
+        sqrt_dt = np.sqrt(dt)
+        #print(lv.shape)
+        S *= np.exp(- 0.5*lv*dt + np.sqrt(lv)*rnd[:,0]*sqrt_dt)
+        return x_
 
 class HestonModel:
     def __init__(self, long_run_variance, mean_reversion_speed, vol_of_vol, 
@@ -107,12 +138,47 @@ class HestonLocalVol:
                     x_strikes: np.array,
                     time_grid: np.array, 
                     n_sims, 
-                    local_var: np.ndarray=None,
-                    x0 = 1.0):
+                    local_var: np.ndarray=None):
         self._stoch_local_variance = HestonLocalVol._calibrate_MC(self._heston, vol_param,
                                 x_strikes, time_grid,  n_sims, local_var)
         self._x_strikes = x_strikes
         self._time_grid = time_grid
+
+    def apply_mc_step(self, x: np.ndarray, t0: float, t1: float, rnd: np.ndarray, inplace: bool = True):
+        """Apply a MC-Euler step for the Heston Local Vol Model for n different paths.
+
+        Args:
+            x (np.ndarray): 2-d array containing the start values for the spot and variance. The first column contains the spot, the second the variance values.
+            t0 ([type]): [description]
+            t1 ([type]): [description]
+            rnd ([type]): [description]
+        """
+        if not inplace:
+            x_ = x.copy()
+        else:
+            x_ = x
+        t0_index = bisect.bisect_left(self._time_grid, t0)
+        if t0_index == 0:# or t0_index == self._time_grid.shape[0]:
+            slv = self._stoch_local_variance[0]
+        elif t0_index == self._time_grid.shape[0]:
+            slv = self._stoch_local_variance[-1]
+        else:
+            dt = self._time_grid[t0_index] - self._time_grid[t0_index-1]
+            w1 = (t0-self._time_grid[t0_index-1])/dt
+            w2 = (self._time_grid[t0_index] - t0)/dt
+            slv = w1*self._stoch_local_variance[t0_index] + w2*self._stoch_local_variance[t0_index-1]
+        slv = np.interp(x[:,0], self._x_strikes, slv)
+        rnd_S = rnd[:,0]
+        rnd_V = rnd[:,1]
+        rnd_corr_S = np.sqrt(1.0-self._heston._correlation**2)*rnd_S + self._heston._correlation*rnd_V
+        S = x_[:,0]
+        v = x_[:,1]
+        dt = t1-t0
+        sqrt_dt = np.sqrt(dt)
+        S *= np.exp(-0.5*v*slv*dt + np.sqrt(v*slv)*rnd_corr_S*sqrt_dt)
+        v += self._heston._mean_reversion_speed*(self._heston._long_run_variance-v)*dt + self._heston._vol_of_vol*np.sqrt(v)*rnd_V*sqrt_dt
+        x_[:,1] = np.maximum(v,0)
+        return x_
 
 
     @staticmethod
@@ -133,10 +199,9 @@ class HestonLocalVol:
             v = x[:,1]
             dt = t1-t0
             sqrt_dt = np.sqrt(dt)
-            x = (0.5*v*slv)*dt + np.sqrt(v*slv)*rnd_corr_S*sqrt_dt
-            S *= np.exp(x)
+            S *= np.exp((0.5*v*slv)*dt + np.sqrt(v*slv)*rnd_corr_S*sqrt_dt)
             v += heston._mean_reversion_speed*(heston._long_run_variance-v)*dt + heston._vol_of_vol*np.sqrt(v)*rnd_V*sqrt_dt
-            v = np.max(v,0)
+            x[:,1] = np.maximum(v,0)
 
         if local_var is None:
             local_var = LocalVol.compute_local_var(vol_param, x_strikes, time_grid)
@@ -152,18 +217,10 @@ class HestonLocalVol:
         for i in range(1,time_grid.shape[0]):
             rnd = np.random.normal(size=(n_sims,2))
             apply_mc_step(x, time_grid[i-1], time_grid[i], rnd, stoch_local_variance[i-1])
-            kr = kernel_regression.KernelRegression().fit(x[:,0:1],x[:,1])
-            stoch_local_variance[i] = kr.predict(x_strikes.reshape((-1,1)))
-            
-            
+            gamma = ((4.0*np.std(x[:,0])**5)/x.shape[0])**(-1.0/5.0)
+            kr = kernel_regression.KernelRegression(gamma = gamma).fit(x[:,0:1],x[:,1])
+            stoch_local_variance[i] = local_var[i]/kr.predict(x_strikes.reshape((-1,1)))
         return stoch_local_variance
-
-    @staticmethod    
-    def apply_mc_step(ln_x0, x0, t0, t1, rnd, model):
-        drift, vol = model.get_SDE_coeff(t0, x0)
-        #print(vol)
-        #ln_x0 += drift*(t1-t0) + vol*np.sqrt(t1-t0)*rnd
-        return ln_x0 + drift*(t1-t0) + vol*np.sqrt(t1-t0)*rnd
 
 if __name__=='__main__':
     import rivapy.marketdata as mktdata
@@ -173,33 +230,40 @@ if __name__=='__main__':
     #LocalVol.compute_local_var(ssvi, x_strikes, time_grid)
 
     import pickle
-    with open('~/doeltz/development/RiVaPy/notebooks/models/depp.pck', 'rb') as f:
+    with open('C:/Users/Anwender/development/RIVACON/RiVaPy/notebooks/models/depp.pck', 'rb') as f:
         heston_grid_param = pickle.load(f)
-    heston = HestonModel(long_run_variance=0.3**2, 
-                            mean_reversion_speed=0.5, 
-                            vol_of_vol=0.2, 
-                            initial_variance=0.1**2, 
-                            correlation = -0.9)
-    x_strikes = np.linspace(0.7, 1.3, 50)
-    time_grid = np.linspace(0.0, 1.0, 80)
-    heston_lv = HestonLocalVol(heston)
-    heston_lv.calibrate_MC(heston_grid_param,  x_strikes=x_strikes, time_grid=time_grid, n_sims=1000) 
-
-    # ssvi = mktdata.VolatilityParametrizationSSVI(expiries=[1.0/365, 30/365, 0.5, 1.0], fwd_atm_vols=[0.25, 0.3, 0.28, 0.25], rho=-0.9, eta=0.5, gamma=0.5)
+    heston_grid_param._pyvacon_obj = None
+    # heston = HestonModel(long_run_variance=0.3**2, 
+    #                         mean_reversion_speed=0.5 , 
+    #                         vol_of_vol=0.2, 
+    #                         initial_variance=0.1**2, 
+    #                         correlation = -0.9)
     # x_strikes = np.linspace(0.7, 1.3, 50)
     # time_grid = np.linspace(0.0, 1.0, 80)
-    # heston = HestonModel(long_run_variance=0.2**2, 
-    #                         mean_reversion_speed=2.0, 
-    #                         vol_of_vol=0.1, 
-    #                         initial_variance=0.1**2, 
-    #                         correlation = -0.9, 
-    #                         drift_stock=lambda x: 0)
-    # heston_lv = HestonLocalVol.calibrate_MC(heston, ssvi,  x_strikes=x_strikes, 
-    #                                             time_grid=time_grid, drift_stock=np.zeros(time_grid.shape),
-    #                                             n_sims=1000) 
+    # heston_lv = HestonLocalVol(heston)
+    # heston_lv.calibrate_MC(heston_grid_param,  x_strikes=x_strikes, time_grid=time_grid, n_sims=1000)
 
-    # import matplotlib.pyplot as plt
-    # time_index = 0
-    # plt.plot(x_strikes, heston_lv._stoch_local_variance[time_index])
-    # plt.plot(x_strikes, heston_lv._local_vol._local_variance[time_index])
-    # plt.show()
+    n_sims=100_000
+    strikes = np.linspace(0.7, 1.3, 50)
+    timegrid = np.linspace(0.0,3.0,3*365)
+    expiries = timegrid[10::30]
+
+    local_vol = LocalVol(heston_grid_param, np.linspace(0.7,1.3, 120), timegrid)
+    simulated_values = np.empty((n_sims, 1))
+    simulated_values[:,0] = 1.0
+
+    paths = np.empty((timegrid.shape[0], n_sims, 1))
+    paths[0] = simulated_values
+
+    for i in range(1,timegrid.shape[0]):
+        rnd = np.random.normal(size=(n_sims,1))
+        paths[i] = local_vol.apply_mc_step(simulated_values, timegrid[i-1], timegrid[i], rnd, inplace=True)
+        depp = local_vol.apply_mc_step(simulated_values, timegrid[i-1], timegrid[i], rnd, inplace=True)
+        if isinstance(depp,tuple):
+            break
+    call_prices_local_vol = np.empty((expiries.shape[0], strikes.shape[0]))
+    p = paths[10::30]
+
+    for i in range(expiries.shape[0]):
+        for k in range(strikes.shape[0]):
+            call_prices_local_vol[i,k] = np.mean(np.maximum(p[i][:,0]-strikes[k],0.0))

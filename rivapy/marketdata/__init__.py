18c5b85b7596e3f69559f36518c7a6f95c48cb19
traktor31 <n.teschner@rivacon.com>
2021-09-17 16:11:18 +0200

    admin

--- rivapy/marketdata/__init__.py
+++ rivapy/marketdata/__init__.py
@@ -166,16 +166,30 @@ class VolatilityParametrizationSABR:
         return self._x[4*expiry:4*(expiry+1)]
     
     def calc_implied_vol(self, ttm, strike):
+        i = np.searchsorted(self.expiries, ttm)
+        if i == 0 or i == self.expiries.shape[0]:
+            if i == self.expiries.shape[0]:
+                i -= 1
+                
+            return self._s(i,ttm, strike)
+        s0 = self._s(i-1,ttm,strike)
+        s1 = self._s(i,ttm, strike)
+        #linear n total variance
+        delta_t = self.expiries[i]-self.expiries[i-1]
+        s = ((self.expiries[i]-ttm)*s0 + (ttm-self.expiries[i-1])*s1)/delta_t
+        return s
+    
+    def _s(self, i: int, ttm: float, strike: float):
         K = strike
-        alpha = self.get_params_at_expiry(ttm)[0] 
-        ny = self.get_params_at_expiry(ttm)[1]
-        beta = self.get_params_at_expiry(ttm)[2]
-        rho = self.get_params_at_expiry(ttm)[3]
+        alpha = self.get_params_at_expiry(i)[0] 
+        ny = self.get_params_at_expiry(i)[1]
+        beta = self.get_params_at_expiry(i)[2]
+        rho = self.get_params_at_expiry(i)[3]
         f = 1
         
         zeta = ny/alpha*(f*K)**((1-beta)/2)*np.log(f/K)
         chi_zeta = np.log((np.sqrt(1-2*rho*zeta+zeta**2)+zeta-rho)/(1-rho))
-
+        
         if f == K:
             sigma = alpha*(1+((1-beta)**2/24*alpha**2/f**(2-2*beta)+1/4*rho*beta*ny*alpha/f**(1-beta)+(2-3*rho**2)/24*ny**2)*ttm)/f**(1-beta)
 
@@ -183,6 +197,7 @@ class VolatilityParametrizationSABR:
             sigma = alpha*(1+((1-beta)**2/24*alpha**2/(f*K)**(1-beta)+1/4*rho*beta*ny*alpha/(f*K)**((1-beta)/2)+(2-3*rho**2)/24*ny**2)*ttm)/(f*K)**((1-beta)/2)*(1+(1-beta)**2/24*np.log(f/K)**2+(1-beta)**4/1920*np.log(f/K)**4)*zeta/chi_zeta
 
         return sigma
+        
 
     def _get_x(self, sabr_params)->np.array:
         x = np.empty(len(sabr_params)*4)
@@ -235,11 +250,11 @@ class VolatilitySurface:
         else:
             expiries = np.linspace(0.0, 4.0, 13, endpoint=True)
         strikes = np.linspace(0.4, 1.6, num=100)
-        vols = np.empty(expiries.shape[0], strikes.shape[0])
-        for i in range(expiries.shape[0]):
-            for j in range(expiries.shape[0]):
-                vols[i,j] = vol_param.calc_implied_vol(expiries[i], vols[j])
-        return VolatilityGridParametrization(expiries, strikes, vols)
+        vols = np.empty((expiries.size, strikes.size))
+        for i in range(expiries.size):
+            for j in range(strikes.size):
+                vols[i,j] = vol_param.calc_implied_vol(expiries[i], strikes[j])
+        return VolatilityGridParametrization(expiries, strikes, vols)._get_pyvacon_obj()
 
     def __init__(self, id: str, refdate: datetime, forward_curve, daycounter, vol_param):
         """Volatility surface
@@ -264,7 +279,7 @@ class VolatilitySurface:
                 fwd_curve = self.forward_curve
             self._pyvacon_obj = _mkt_data.VolatilitySurface(self.id, self.refdate,
                 fwd_curve._get_pyvacon_obj(),self.daycounter.name, 
-                VolatilitySurface._get_pyvacon_obj(self.vol_param))
+                VolatilitySurface._create_param_pyvacon_obj(self.vol_param))
         return self._pyvacon_obj
     
     def calc_implied_vol(self,  expiry: datetime, strike: float, refdate: datetime = None, forward_curve=None)->float:
--- tests/test_marketdata.py
+++ tests/test_marketdata.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime as dt
 
 import rivapy
-from rivapy.marketdata import VolatilityGridParametrization, VolatilitySurface, DiscountCurve, EquityForwardCurve, enums
+from rivapy.marketdata import VolatilityGridParametrization, VolatilityParametrizationSABR, VolatilitySurface, DiscountCurve, EquityForwardCurve, enums
 from rivapy import enums
 
 class VolatilityGridParamTest(unittest.TestCase):
@@ -33,6 +33,26 @@ class VolatilityGridParamTest(unittest.TestCase):
         vol_surface = VolatilitySurface('', refdate, fwd, enums.DayCounterType.Act365Fixed, vol_grid_param)
         vol = vol_surface.calc_implied_vol(refdate+dt.timedelta(days=365), 100.0)
         self.assertAlmostEqual(vol, vol_grid_param.calc_implied_vol(1.0, 1.3), delta=1e-7)
+        
+        
+class VolatilitySABRParamTest(unittest.TestCase):
+    
+    def test_calc_implied_vol(self):
+        # 1. Parametrization
+        expiries = [1.0/12.0, 1.0, 2.0, 3.0]
+        sabr_params = np.array([[.1, 0.1, .9,-.8], [.3, 0.1, .1, .1], [.5, .3, .9, -.75,], [.5, .3, .9, -.85,]])
+        sabr_param = VolatilityParametrizationSABR(expiries, sabr_params)
+        self.assertAlmostEqual(0.30118,sabr_param.calc_implied_vol(ttm = 1.0,strike = 1.0), delta=1e-7)
+        
+        # 2. Vol Surface
+        obj_id = 'Test Surface'
+        refdate = dt.datetime(2021,1,1)
+        dc = DiscountCurve('',refdate=refdate, dates=[refdate, refdate+dt.timedelta(days=10*365)], df=[1.0,1.0])
+        fc = EquityForwardCurve(100.0, funding_curve=dc, borrow_curve=dc,div_table=None)
+        vol_surf = VolatilitySurface(obj_id, refdate, fc, enums.DayCounterType.Act365Fixed, sabr_param)
+        vol = vol_surf.calc_implied_vol(refdate+dt.timedelta(days=365), 100.0,refdate)
+        self.assertAlmostEqual(vol, sabr_param.calc_implied_vol(ttm = 1.0,strike = 1.0), delta=1e-7)
+        
 
 if __name__ == '__main__':
     unittest.main()

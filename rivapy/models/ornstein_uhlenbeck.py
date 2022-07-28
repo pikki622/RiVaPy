fa4a41fe873e2f1a1e2638a42824883a40e38d01
Daniel Oeltz <d.oeltz@rivacon.com>
2022-07-28 07:58:03 +0200

    add calibration

--- rivapy/models/ornstein_uhlenbeck.py
+++ rivapy/models/ornstein_uhlenbeck.py
@@ -9,8 +9,10 @@ class OrnsteinUhlenbeck:
             return result
 
     def __init__(self, speed_of_mean_reversion: Union[float, Callable], 
-                    volatility: Union[float, Callable]):
+                    volatility: Union[float, Callable], 
+                    mean_reversion_level: Union[float, Callable] = 0):
         self.speed_of_mean_reversion = speed_of_mean_reversion
+        self.mean_reversion_level = mean_reversion_level,
         self.volatility = volatility
         self._timegrid = None
 
@@ -21,13 +23,18 @@ class OrnsteinUhlenbeck:
 
         self._speed_of_mean_reversion = OrnsteinUhlenbeck._eval_grid(self.speed_of_mean_reversion, timegrid)
         self._volatility = OrnsteinUhlenbeck._eval_grid(self.volatility, timegrid)
+        self._mean_reversion_level = OrnsteinUhlenbeck._eval_grid(self.mean_reversion_level, timegrid)
         
     def simulate(self, timegrid, start_value, rnd):
         self._set_timegrid(timegrid)
         result = np.empty((self._timegrid.shape[0], rnd.shape[0]))
-        result[0,:] = start_value 
+        result[0,:] = start_value
+
         for i in range(self._timegrid.shape[0]-1):
-            result[i+1,:] = (1.0  - self._speed_of_mean_reversion[i]*self._delta_t[i])*result[i,:] + self._volatility[i]*self._sqrt_delta_t[i]*rnd[:,i]    
+            result[i+1,:] = (result[i, :] * np.exp(-self._speed_of_mean_reversion[i]*self._delta_t[i])
+                        + self._mean_reversion_level[i]* (1 - np.exp(-self._speed_of_mean_reversion[i]*self._delta_t[i])) 
+                        + self._volatility[i]* np.sqrt((1 - np.exp(-2*self._speed_of_mean_reversion[i]*self._delta_t[i])) / (2*self._speed_of_mean_reversion[i])) * rnd[:,i]
+                        )
         return result
 
 
@@ -56,11 +63,13 @@ class OrnsteinUhlenbeck:
     def conditional_probability_density(self, X_delta_t, delta_t, X0, 
                                         volatility=None, 
                                         speed_of_mean_reversion=None, 
-                                        mean_reversion_level = 0):
+                                        mean_reversion_level = None):
         if volatility is None:
             volatility = self.volatility
         if speed_of_mean_reversion is None:
             speed_of_mean_reversion = self.speed_of_mean_reversion
+        if mean_reversion_level is None:
+            mean_reversion_level = self.mean_reversion_level
         volatility_2_ = volatility**2*(1.0-np.exp(-2.0*speed_of_mean_reversion*delta_t))/(2.0*speed_of_mean_reversion)
         result = 1.0/(2.0*np.pi*volatility_2_)*np.exp(
                         -(X_delta_t-X0*np.exp(-speed_of_mean_reversion*delta_t)
@@ -68,4 +77,38 @@ class OrnsteinUhlenbeck:
                     /(2.0*volatility_2_))
         return result
 
-    
+    def calibrate(self, data: np.ndarray, dt: float, method: str='maximum_likelihood'):
+        """Calibrate the Ornstein Uhlenbeck model with constant parameters to the given data.
+
+        Args:
+            data (np.ndarray): Array of values the model is fitted to (uniform timegrid is assumed).
+            dt (float): Time step size between two datapoints from data (uniform timegrid is assumed.
+            method (str, optional): Determines if maximum likelihood ('maximum_likelihood') or minimum least square ('minimum_least_square') is used for calibration. Defaults to 'maximum_likelihood'.
+        """
+        Sx  = (data[:-1]).sum()
+        Sy  = (data[1:]).sum()
+        Sxx = (data[:-1]**2).sum()
+        Syy = (data[1:]**2).sum()
+        Sxy = (data[:-1] * data[1:]).sum()
+        n = data[:-1].shape[0] 
+        if method == 'maximum_likelihood':        
+            mu = (Sy*Sxx - Sx*Sxy) / (n*(Sxx-Sxy) - (Sx**2-Sx*Sy))
+            if ((Sxy-mu*(Sx+Sy-n*mu))/(Sxx-2*mu*Sx+n*mu**2)) <= 0:
+                raise Exception('Calibration failed.')
+            speed_mr = -1/dt * np.log((Sxy-mu*(Sx+Sy-n*mu))/(Sxx-2*mu*Sx+n*mu**2)) 
+            alpha = np.exp(-speed_mr*dt)
+            sigma_2 = 1/n * (Syy-2*alpha*Sxy+alpha**2*Sxx-2*mu*(1-alpha)*(Sy-alpha*Sx)+n*mu**2*(1-alpha)**2)
+            sigma = np.sqrt(sigma_2 * (2*speed_mr) / (1-alpha**2))          
+        elif method == 'minimum_least_square':
+            a = (n*Sxy - Sx*Sy) / (n*Sxx - Sx**2)
+            b = (Sy - a*Sx) / n
+            sd = np.sqrt((n*Syy-Sy**2 - a*(n*Sxy-Sx*Sy)) / (n*(n-2)))
+            speed_mr = -np.log(a) / dt
+            mu = b / (1-a)
+            sigma = sd * np.sqrt((-2*np.log(a)) / (dt*(1-a**2)))
+        else:
+            raise ValueError('Fitting method not defined ('+method+')')        
+        self.speed_of_mean_reversion = speed_mr
+        self.volatility = sigma
+        self.mean_reversion_level = mu
+#

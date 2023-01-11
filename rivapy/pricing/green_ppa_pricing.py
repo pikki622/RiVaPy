dc930c8693aca0b67ad6eb831937336603774123
Daniel Oeltz <d.oeltz@rivacon.com>
2023-01-11 06:58:44 +0100

    admin

--- rivapy/pricing/green_ppa_pricing.py
+++ rivapy/pricing/green_ppa_pricing.py
@@ -1,4 +1,4 @@
-from typing import Protocol
+from typing import List
 try:
     import tensorflow as tf
     tf.config.run_functions_eagerly(False)
@@ -48,7 +48,11 @@ class PPAHedgeModel(tf.keras.Model):
         self._prev_q = tf.zeros((tf.shape(power_fwd)[0]), name='prev_q')
         for i in range(self.timegrid.shape[0]-2):
             t = [self.timegrid[i]]*tf.ones((tf.shape(power_fwd)[0],1))/self.timegrid[-1]
-            quantity = tf.squeeze(self.model([power_fwd[:,i], forecast[:,i], t], training=training))
+            inputs = [v[:,i] for v in x]
+            inputs.append(t)
+            #quantity = tf.squeeze(self.model([power_fwd[:,i], forecast[:,i], t], training=training))
+            quantity = tf.squeeze(self.model(inputs, training=training))
+            power_fwd
             pnl = pnl + tf.math.multiply((self._prev_q-quantity), tf.squeeze(power_fwd[:,i]))
             self._prev_q = quantity
         pnl = pnl + self._prev_q* tf.squeeze(power_fwd[:,-1]) + rlzd_qty[:,-1]*(tf.squeeze(power_fwd[:,-1])-self.strike)
@@ -69,14 +73,22 @@ class PPAHedgeModel(tf.keras.Model):
             callbacks.append(tensorboard_callback)
         self.compile(optimizer=optimizer, loss=self.custom_loss)
         y = np.zeros((paths_fwd_price.shape[0],1))
-        return self.fit([paths_fwd_price, paths_forecasts, rlzd_qty], y, epochs=epochs, 
+        inputs = [paths_fwd_price]
+        for k,v in paths_forecasts.items():
+            inputs.append(v)
+        inputs.append(rlzd_qty)
+        return self.fit(inputs, y, epochs=epochs, 
                             batch_size=batch_size, callbacks=callbacks, verbose=verbose)
 
-def _build_model(depth, nb_neurons):
-    power_fwd_price = tf.keras.Input(shape=(1,),name = "power_fwd_price")
-    forecast = tf.keras.Input(shape=(1,),name = "forecast")
-    t = tf.keras.Input(shape=(1,),name = "t")
-    fully_connected_Input = tf.keras.layers.concatenate([power_fwd_price, forecast, t])         
+def _build_model(depth, nb_neurons, regions: List[str] = None):
+    inputs= [tf.keras.Input(shape=(1,),name = "power_fwd_price")]
+    if regions is None:
+        inputs.append(tf.keras.Input(shape=(1,),name = "forecast"))
+    else:
+        for r in regions:
+            inputs.append(tf.keras.Input(shape=(1,),name = "forecast_"+r))
+    inputs.append(tf.keras.Input(shape=(1,),name = "t"))
+    fully_connected_Input = tf.keras.layers.concatenate(inputs)         
     values_all = tf.keras.layers.Dense(nb_neurons,activation = "selu", 
                     kernel_initializer=tf.keras.initializers.GlorotUniform())(fully_connected_Input)       
     for _ in range(depth):
@@ -84,7 +96,7 @@ def _build_model(depth, nb_neurons):
                     kernel_initializer=tf.keras.initializers.GlorotUniform())(values_all)            
     value_out = tf.keras.layers.Dense(1, activation="linear",
                     kernel_initializer=tf.keras.initializers.GlorotUniform())(values_all)
-    model = tf.keras.Model(inputs=[power_fwd_price, forecast, t], outputs = value_out)
+    model = tf.keras.Model(inputs=inputs, outputs = value_out)
     return model
 
 class PricingResults:
@@ -105,7 +117,6 @@ def _validate(val_date: dt.datetime,
     if green_ppa.n_deliveries() > 1:
         raise Exception('Pricer for more than one delivery not yet implemented.')
 
- 
 def price( val_date: dt.datetime,
             green_ppa: GreenPPASpecification,
             power_wind_model: ResidualDemandForwardModel, 
@@ -134,6 +145,44 @@ def price( val_date: dt.datetime,
     k = list(forecasts.keys())[0]
     forecasts =  np.squeeze(forecasts[k].transpose())
     #######################
+    hedge_model = PPAHedgeModel(model, timegrid.timegrid, None, regularization, strike=green_ppa.fixed_price)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=initial_lr,#1e-3,
+            decay_steps=100*fwd_prices.shape[0]/batch_size,
+            decay_rate=decay_rate)
+    hedge_model.train(fwd_prices, forecasts, rlzd_qty, lr_schedule, epochs, batch_size, 
+                        tensorboard_log=tensorboard_logdir, verbose=verbose)
+    return PricingResults(hedge_model, timegrid, fwd_prices, forecasts)
+ 
+def price_new( val_date: dt.datetime,
+            green_ppa: GreenPPASpecification,
+            power_wind_model: ResidualDemandForwardModel, 
+            depth: int, nb_neurons: int, 
+            n_sims: int, regularization: float, 
+            epochs: int,
+            verbose: bool=0,
+            tensorboard_logdir: str=None, initial_lr: float = 1e-4, 
+            batch_size: int = 100, decay_rate: float=0.7, seed: int = 42):
+    #print(locals())
+    _validate(val_date, green_ppa,power_wind_model)
+    ppa_schedule = green_ppa.get_schedule()
+    if ppa_schedule[-1] <= val_date:
+        return None
+    tf.random.set_seed(seed)
+    timegrid = DateTimeGrid(start=val_date, end=ppa_schedule[-1], freq='1H')
+    rnd = np.random.normal(size=power_wind_model.rnd_shape(n_sims, timegrid.timegrid.shape[0]))
+    fwd_prices, forecasts = power_wind_model.simulate(timegrid, rnd)
+    rlzd_qty = power_wind_model.compute_rlzd_qty(green_ppa.location, forecasts)
+    
+    fwd_prices = np.squeeze(fwd_prices.transpose())
+    
+    #print(fwd_prices.mean(axis=0))
+    # dirty hack to test!!!
+    regions = list(forecasts.keys())[0]
+    forecasts =  np.squeeze(forecasts[k].transpose())
+    #######################
+    model = _build_model(depth, nb_neurons, regions)
+    
     hedge_model = PPAHedgeModel(model, timegrid.timegrid, None, regularization, strike=green_ppa.fixed_price)
     lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
             initial_learning_rate=initial_lr,#1e-3,

e398ed0db13f759596ac93e30c1469f4a4f4c612
Daniel Oeltz <d.oeltz@rivacon.com>
2023-02-02 15:21:53 +0100

    add logger to sample_data

--- rivapy/sample_data/market_data/spread_curves.py
+++ rivapy/sample_data/market_data/spread_curves.py
@@ -1,8 +1,9 @@
 import numpy as np
-from rivapy.tools.enums import Currency, ESGRating, Rating, Sector, Country
-from rivapy.marketdata import DiscountCurveParametrized, NelsonSiegel, LinearRate
+from rivapy.tools.enums import Currency, ESGRating, Rating, Sector, Country, SecuritizationLevel
+from rivapy.marketdata import DiscountCurveParametrized, NelsonSiegel, LinearRate, ConstantRate
 from rivapy.instruments.components import Issuer
 from rivapy.instruments import PlainVanillaCouponBondSpecification
+from rivapy.sample_data._logger import logger
 
 class SpreadCurveSampler:
     def __init__(self):
@@ -12,6 +13,9 @@ class SpreadCurveSampler:
             * currency (for all currencies defined in :class:`rivapy.tools.enums.Currency`)
             * esg rating (for all ratings defined in :class:`rivapy.tools.enums.ESGRating`)
             * sector (for all sectors defined in :class:`rivapy.tools.enums.Sector`)
+            * securitization level (only SENIOR_SECURED, SENIOR_UNSECURED and SUBORDINATED are currently handled)
+
+            Note that this does not include securitization levels at the moment.
         """
         pass
     
@@ -45,7 +49,8 @@ class SpreadCurveSampler:
         self._sample_esg_rating_spreads()
         self._sample_rating_weights()
         self._sample_sector_spreads()
-        self._sample_country_curves()
+        self._sample_country_curves(ref_date=ref_date)
+        self._sample_sec_level_spreads()
         
         
     def _sample_currency_spread(self):
@@ -83,7 +88,7 @@ class SpreadCurveSampler:
             result[s.value] = (s_low, s_low+np.random.uniform(low=0.001, high=0.0025))
         self.sector_spreads = result
         
-    def _sample_country_curves(self):
+    def _sample_country_curves(self, ref_date):
         self.country_curves = {}
         for c in Country:
             shortterm_rate = np.random.uniform(low=0.0, high=0.02)
@@ -94,7 +99,18 @@ class SpreadCurveSampler:
                                                                                        )
                                   )
     
+    def _sample_sec_level_spreads(self):
+        result = {}
+        spread = 0.0
+        result[SecuritizationLevel.SENIOR_SECURED.value]=(0.0,0.001)
+        low = np.random.uniform(0.001, 0.005)
+        result[SecuritizationLevel.SENIOR_UNSECURED.value] = (low, low + 0.01)
+        low = np.random.uniform(0.01, 0.025) + result[SecuritizationLevel.SENIOR_UNSECURED.value][1]
+        result[SecuritizationLevel.SUBORDINATED.value] = (low, low + 0.03)
+        self.securitization_spreads = result
+        
     def get_curve(self, issuer: Issuer, bond: PlainVanillaCouponBondSpecification):
+        logger.info('computing curve for issuer ' + issuer.name + ' and bond ' + bond.obj_id)
         rating_weight = self.rating_weights[issuer.rating]
         w1 = 1.0-rating_weight
         w2 = rating_weight
@@ -103,5 +119,7 @@ class SpreadCurveSampler:
         esg_spread = w1*self.esg_rating_spread[issuer.esg_rating][0] +  w2*self.esg_rating_spread[issuer.esg_rating][1]
         sector_spread = w1*self.sector_spreads[issuer.sector][0] + w2*self.sector_spreads[issuer.sector][1]
         currency_spread = w1*self.currency_spread[bond.currency][0] + w2*self.currency_spread[bond.currency][1]
-        curve = 0.5*rating_curve + 0.5*(0.3*country_spread + 0.4*esg_spread + 0.2*sector_spread+0.1*currency_spread)
+        securitization_spread = w1*self.securitization_spreads[bond.securitization_level][0] + w2* self.securitization_spreads[bond.securitization_level][1]
+        curve = 0.5*rating_curve + 0.5*(0.3*country_spread + 0.3*securitization_spread 
+                        + 0.2*esg_spread + 0.1*sector_spread+0.1*currency_spread)
         return curve
